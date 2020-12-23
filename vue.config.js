f4d164724d2b46ad1e1f0de26b82d6c862fa09b6
fcj253 <3306641479@qq.com>
2020-12-23 12:45:17 +0800

    bug clear

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 module.exports =  {
     devServer : {
+        publicPath : '/miaomiao',
         host: '0.0.0.0',
         public:'192.168.43.123:8080',
         port: 8080,
         https: false,
         hotOnly: false,
         disableHostCheck:true,
-        open: true, // 配置自动启动浏览器
+        open: false, // 配置自动启动浏览器
         proxy : {
             '/ajax' : {
                 target : 'https://m.maoyan.com',
