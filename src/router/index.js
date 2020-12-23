acc31cdd458f3fda4e508ab20f94084258e21679
fcj253 <3306641479@qq.com>
2020-12-23 14:10:11 +0800

    bug clear

--- src/router/index.js
+++ src/router/index.js
@@ -18,8 +18,8 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
-  // base : 'miaomiao',
+  // base: process.env.BASE_URL,
+  base : 'miaomiao',
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 module.exports =  {
     devServer : {
         publicPath : '/miaomiao',
-        host: '0.0.0.0',
-        public:'192.168.43.123:8080',
-        port: 8080,
-        https: false,
-        hotOnly: false,
-        disableHostCheck:true,
-        open: false, // 配置自动启动浏览器
+        // host: '0.0.0.0',
+        // public:'192.168.43.123:8080',
+        // port: 8080,
+        // https: false,
+        // hotOnly: false,
+        // disableHostCheck:true,
+        // open: false, // 配置自动启动浏览器
         proxy : {
             '/ajax' : {
                 target : 'https://m.maoyan.com',
